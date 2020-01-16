3bdab698d6718eefa53af5f7455c1434dfdf86dc
NESTOWL <135sasha135@mail.ru>
2020-01-16 17:37:14 +0300

    начало положено

---
+++ быдло_бот.py
@@ -0,0 +1,18 @@
+
+from discord.ext import commands
+
+
+я_твой_рот_ебал = commands.Bot(command_prefix= "яхуесос")
+
+это_ебаное_событие_века = я_твой_рот_ебал.event
+эта_ебаная_команда = я_твой_рот_ебал.command
+
+@это_ебаное_событие_века
+async def on_ready():
+	print("я запустился! А ты нахуй иди, падаль")
+
+@эта_ебаная_команда(пасс_контекст='да я далбаеб')
+async def хуета(хуета):
+	await хуета.send('я сасал меня ебали и тебя тоже!')
+
+я_твой_рот_ебал.run('NjYwODk0ODI0MjUbMzU4MTEw.XfjgWA.554gJESNiUan1r0B2zE-r0dQGes')
